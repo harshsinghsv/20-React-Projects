22e975ca801975a376e695aadc4834cfd678c062
Harsh Singh <harshsingh15dec@gmail.com>
2025-08-21 03:29:00 +0530

    Card Button Shadow added

--- 0-Profile-Card/src/components/ProfileCard.jsx
+++ 0-Profile-Card/src/components/ProfileCard.jsx
@@ -7,28 +7,33 @@ import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
 const ProfileCard = ({name, role, img, buttonText, followers, following}) => {
   return (
-    <div className='h-96 w-60 bg-gray-800 shadow-md rounded-3xl overflow-hidden p-1 font-roboto font-bold'>
-        <div className='w-full h-[60%] overflow-hidden rounded-3xl'>
+    <div className='h-96 w-[260px] bg-[#2f2f2f] rounded-3xl overflow-hidden p-2 font-light font-jakarta shadow-xl/30'>
+        <div className='w-full h-[65%] overflow-hidden rounded-3xl'>
             <img src={img} alt={name} className='w-full h-full rounded-3xl mx-auto' />
         </div>
         
-        <div className='flex flex-col gap-1.5 justify-start ml-2 mr-2 mt-3'>
+        <div className='flex flex-col gap-1.5 justify-start ml-2 mr-2 mt-4'>
             <h2 className='text-[18px] font-semibold text-white'>{name}</h2>
-            <p className='text-[12px] text-gray-300'>{role}</p>
-            <div className='flex items-center justify-center gap-2'>
+            <p className='text-[12px] text-gray-200'>{role}</p>
+            <div className='flex items-center justify-between gap-3'>
+
+                <div className='flex gap-2'>
                 <div className='flex gap-1 justify-center items-center text-white text-[12px]'>
                     <FontAwesomeIcon icon={faUser} />
                     {followers}
-                </div>
-                <div className='flex gap-1 justify-center items-center text-white text-[12px] '>
+                </div>    
+                
+                <div className='flex gap-1 justify-between items-center text-white text-[12px]'>    
                     <FontAwesomeIcon icon={faUserPlus} />
                     {following}
 
                 </div>
+                </div>
                 <div>
-                    <button className='px-3 py-2 bg-gray-700 text-white text-[14px] rounded-full flex gap-1 justify-center items-center'>
-                        {buttonText}
+                    <button className='px-3 py-2 bg-[#2f2f2f] text-white text-[12px] rounded-full flex gap-1 justify-center items-center inset-shadow-sm inset-shadow-white/10 '>
                         <FontAwesomeIcon icon={faPlus} />
+                        {buttonText}
+
                     </button>
                 </div>
             </div>
