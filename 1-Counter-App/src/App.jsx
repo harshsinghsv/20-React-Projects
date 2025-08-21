c9a3651e0a820fbeb5bef1a1029a0d9622e4ae4c
Harsh Singh <harshsingh15dec@gmail.com>
2025-08-21 23:08:52 +0530

    App Updated

---
+++ 1-Counter-App/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CounterApp from "./components/CounterApp";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <CounterApp />
+    </div>
+  );
+}
+
+export default App;
